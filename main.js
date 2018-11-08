2007b67b67c77aa544cb2c24485f264cbf133366
Ibanga Enoobong Ime <enoimeibanga@gmail.com>
2018-11-08 08:42:01 +0100

    learnt menu and context menu

--- main.js
+++ main.js
@@ -1,16 +1,189 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, shell, BrowserWindow, Menu, ipcMain, MenuItem } = require('electron')
 
+const contextMenu = new Menu()
+contextMenu.append(new MenuItem({label: 'Cut', role: 'cut' }))
+contextMenu.append(new MenuItem({label: 'Copy', role: 'copy' }))
+contextMenu.append(new MenuItem({label: 'Paste', role: 'paste' }))
+contextMenu.append(new MenuItem({label: 'Select All', role: 'selectall' }))
+contextMenu.append(new MenuItem({type: 'separator' }))
+contextMenu.append(new MenuItem({label: 'Custom', click() { console.log('Custom Menu') }
+}))
+
+ipcMain.on('show-context-menu', function (event) {
+  const win = BrowserWindow.fromWebContents(event.sender)
+  contextMenu.popup(win)
+})
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+let template = [{
+  label: 'Edit',
+  submenu: [{
+    label: 'Undo',
+
+    role: 'undo'
+  }, {
+    label: 'Redo',
+    accelerator: 'Shift+CmdOrCtrl+Z',
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Cut',
+    accelerator: 'CmdOrCtrl+X',
+    role: 'cut'
+  }, {
+    label: 'Copy',
+    accelerator: 'CmdOrCtrl+C',
+    role: 'copy'
+  }, {
+    label: 'Paste',
+    accelerator: 'CmdOrCtrl+V',
+    role: 'paste'
+  }, {
+    label: 'Select All',
+    accelerator: 'CmdOrCtrl+A',
+    role: 'selectall'
+  }, {
+    type: 'separator'
+  }]
+}, {
+  label: 'View',
+  submenu: [{
+    label: 'Reload',
+    accelerator: 'CmdOrCtrl+R',
+    click: function (item, focusedWindow) {
+      if (focusedWindow) {
+        // on reload, start fresh and close any old
+        // open secondary windows
+        if (focusedWindow.id === 1) {
+          BrowserWindow.getAllWindows().forEach(function (win) {
+            if (win.id > 1) {
+              win.close()
+            }
+          })
+        }
+        focusedWindow.reload()
+      }
+    }
+  }, {
+    label: 'Toggle Full Screen',
+    accelerator: (function () {
+      if (process.platform === 'darwin') {
+        return 'Ctrl+Command+F'
+      } else {
+        return 'F11'
+      }
+    })(),
+    click: function (item, focusedWindow) {
+      if (focusedWindow) {
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+      }
+    }
+  }, {
+    label: 'Toggle Developer Tools',
+    accelerator: (function () {
+      if (process.platform === 'darwin') {
+        return 'Alt+Command+I'
+      } else {
+        return 'Ctrl+Shift+I'
+      }
+    })(),
+    click: function (item, focusedWindow) {
+      if (focusedWindow) {
+        focusedWindow.toggleDevTools()
+      }
+    }
+  }]
+}, {
+  label: 'Window',
+  role: 'window',
+  submenu: [{
+    label: 'Minimize',
+    accelerator: 'CmdOrCtrl+M',
+    role: 'minimize'
+  }, {
+    label: 'Close',
+    accelerator: 'CmdOrCtrl+W',
+    role: 'close'
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Reopen Window',
+    accelerator: 'CmdOrCtrl+Shift+T',
+    enabled: false,
+    key: 'reopenMenuItem',
+    click: function () {
+      app.emit('activate')
+    }
+  }]
+}, {
+  label: 'Help',
+  role: 'help',
+  submenu: [{
+    label: 'Learn More',
+    click: function () {
+      shell.openExternal('http://electron.atom.io')
+    }
+  }]
+}]
+
+if (process.platform === 'darwin') {
+  const name = Electron.app.getName()
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: `About ${name}`,
+        role: 'about',
+      },
+      { type: 'separator' },
+      {
+        label: 'Preferences',
+        accelerator: 'Command+,',
+        click: appPrefs
+      },
+      { type: 'separator' },
+      {
+        label: 'Services',
+        role: 'services',
+        submenu: [],
+      },
+      { type: 'separator' },
+      {
+        label: `Hide ${name}`,
+        accelerator: 'Command+H',
+        role: 'hide',
+      }, {
+        label: 'Hide Others',
+        accelerator: 'Command+Alt+H',
+        role: 'hideothers',
+      }, {
+        label: 'Show All',
+        role: 'unhide',
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click: function () {
+          app.quit()
+        }
+      }]
+  })
+}
+
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ show: false, backgroundColor: '#FFF', width: 800, height: 600 })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -27,7 +200,11 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  createWindow()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const { remote, ipcRenderer } = require('electron')
+const ipc = ipcRenderer
+
+window.addEventListener('contextmenu', (event) => {
+    event.preventDefault()
+    ipc.send('show-context-menu')
+})
